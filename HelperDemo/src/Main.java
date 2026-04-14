6d11bdcb22f3617713e8d4f7386df612b7faadac
Saumya-sura <saumya.sura74@nmims.in>
2026-04-14 18:15:58 +0530

    Add AngularJS application for basic arithmetic operations with user input

---
+++ HelperDemo/src/Main.java
@@ -0,0 +1,8 @@
+import helper.Helper;
+
+public class Main {
+    public static void main(String[] args) {
+        int sum = Helper.add(5, 7);
+        System.out.println("Sum: " + sum);
+    }
+}
---
+++ HelperDemo/src/helper/Helper.java
@@ -0,0 +1,7 @@
+package helper;
+
+public class Helper {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
